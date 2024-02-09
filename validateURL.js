a968bc7b91f1aed84fde278a390926f3a4da5eae
bouzenaali <Alibouzena16@gmail.com>
2024-02-09 22:34:46 +0100

    feat: add Figma file accessibility check

--- validateURL.js
+++ validateURL.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const axios = require('axios');
 
 async function checkGitHubRepositoryAccessibility(url) {
@@ -22,5 +23,36 @@ const githubUrls = [
 
 githubUrls.forEach(async (url) => {
     const isPublic = await checkGitHubRepositoryAccessibility(url);
-    console.log(`${url}: ${isPublic ? 'Public' : 'Private'}`);
+    console.log(`${url}: ${isPublic ? 'Public' : 'Private or Invalid'}`);
+});
+
+
+async function checkFigmaFileAccessibility(url) {
+    try {
+        const [, fileKey] = url.match(/figma\.com\/file\/([^/]+)/i);
+
+        const response = await axios.get(`https://api.figma.com/v1/files/${fileKey}`, {
+            headers: {
+                'X-Figma-Token': process.env.FIGMA_API_TOKEN 
+            }
+        });
+
+        const isPublic = response.data.document.public;
+
+        return isPublic;
+    } catch (error) {
+        console.error(`${url}: ${error}`);
+        return false;
+    }
+}
+
+// Test Figma URLs
+const figmaUrls = [
+    'https://www.figma.com/file/YoSuZ1tqQAQX5TkjXkUStL/GDG-Hack?type=design&node-id=0-1&mode=design',
+    'https://www.figma.com/file/efgh456/Another-File'
+];
+
+figmaUrls.forEach(async (url) => {
+    const isPublic = await checkFigmaFileAccessibility(url);
+    console.log(`${url}: ${isPublic ? 'Public' : 'Private or Invalid'}`);
 });
